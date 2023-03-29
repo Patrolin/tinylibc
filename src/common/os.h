173fe4bfbfe344d7f1d526dfdaba06ddd17b8bf0
Patrolin <patrik@balas.cz>
2023-03-29 11:26:47 +0200

    c

--- src/common/os.h
+++ src/common/os.h
@@ -1,6 +1,6 @@
 void osExit(uint return_code);
 void osPrint(const u8* msg, uint count);
 void osPanic(const char* msg);
-void* osAlloc(void* prev_ptr, uint size); // alloc and clear to zero
+void* osAlloc(void* prev_ptr, uint size); // alloc ceil(size/PAGE_SIZE) pages and clear them to zero
 //void osCreateThread(callback f);
 //void osCreateSemaphore(?);
