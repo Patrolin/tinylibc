d1c9e1bc2c6522e0e4601fcbf1b66abe3c362a84
Patrolin <patrik@balas.cz>
2023-03-30 20:22:12 +0200

    c

--- src/common/types/os.h
+++ src/common/types/os.h
@@ -10,24 +10,22 @@
 
 internal void osExit(uint return_code);
 internal void osPrint(const u8* msg, uint count);
+
 internal void osPanic(const char* msg);
 internal void* osPageAlloc(void* prev_ptr, uint size); // alloc ceil(size/PAGE_SIZE) pages and clear them to zero
+#ifdef BUILD_RELEASE
+    #define assert(condition, msg)
+#else
+    #define assert(condition, msg) if (!(condition)) osPanic(msg);
+#endif
+#define _pageAllocAssert(prev_ptr, size, ptr) \
+    assert((ptr != 0), "AllocError: ptr = 0\n"); /* TODO: os does not support realloc */ \
+    assert((prev_ptr == 0) || (ptr == prev_ptr), "AllocError: moved prev_ptr");
+
 internal void osSleep(uint ms);
 internal void osNanosleep(uint ms);
 internal u64 osNanoTime();
-// TODO: internal void osCreateThread(callback f);
-// TODO: internal void osCreateSemaphore(?);
 // TODO: internal u64 osDateTime();
 
-internal void assert(bool32 condition, const char* msg) {
-    #ifndef BUILD_RELEASE
-        if (!condition) osPanic(msg);
-    #endif
-}
-#if BUILD_RELEASE
-    #define _pageAllocAssert(prev_ptr, size, ptr)
-#else
-    #define _pageAllocAssert(prev_ptr, size, ptr) \
-        assert((ptr != 0), "AllocError: ptr = 0\n"); /* TODO: os does not support realloc */ \
-        assert((prev_ptr == 0) || (ptr == prev_ptr), "AllocError: moved prev_ptr");
-#endif
+// TODO: internal void osCreateThread(callback f);
+// TODO: internal void osCreateSemaphore(?);
