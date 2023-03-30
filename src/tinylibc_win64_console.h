b281c4168d6fe0f47eb06de6c2e5e2a1e9aa2ac9
Patrolin <patrik@balas.cz>
2023-03-30 15:29:29 +0200

    simplify win64_console

--- src/tinylibc_win64_console.h
+++ src/tinylibc_win64_console.h
@@ -1,16 +1,2 @@
-#include "common.h"
-//#include "common/crt.h"
-
-static_assert(OS_WIN && BITS_64, "Must compile on 64-bit Windows!");
-
 #define BUILD_CONSOLE 1
-#include "os/windows.h"
-
-int WinMain(HINSTANCE app, HINSTANCE prev_app, LPSTR command, int window_options);
-external int _start() {
-    //crtInit();
-    _winInit();
-    int retCode = WinMain(0, 0, 0, 0);
-    //_DoExit();
-    ExitProcess(retCode);
-}
+#include "tinylibc_win64.h"
