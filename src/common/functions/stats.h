8fd3db2d85aa96f310e739827e3ac5a21a08d890
Patrolin <patrik@balas.cz>
2023-04-18 12:33:01 +0200

    c

--- src/common/functions/stats.h
+++ src/common/functions/stats.h
@@ -13,13 +13,13 @@ internal fixed32 stddev(fixed32 arr[5], fixed32 mean) {
     return sqrt(acc / fixed32{ 5 << 16 });
 }
 
-const u32 LCG_K = 2654435769U;
+const u32 LCG_K = 2654435769U; // 2**32 / PHI
 u32 _lcg_state = 1;
 internal u64 randInt() {
     return (_lcg_state++) * LCG_K;
 }
 internal fixed32 random() {
-    return fixed32{ (u32)(randInt() >> 16) };
+    return fixed32{ (u32)(randInt() & F32_FRACTION_MASK) };
 }
 internal fixed32 randomHalfGaussian() {
     return sqrt(-F32_TWO * ln(random())); // * F32_SQRT_PI ?
